cdb76d00265a3e9564bef76a5b7d1061f7a70ba6
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2025-10-04 18:35:13 +0500

    found the error

--- express.js
+++ express.js
@@ -18,8 +18,8 @@ function sendEmail(name, email, message) {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.EMAIL_USER || 'roziyabegimtuxtayeva@gmail.com',
+      pass: process.env.EMAIL_PASS || 'ratu xnub xbbx keks',
     },
   });
 
